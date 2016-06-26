76f381498c9a395758db67abff9a0a6531937313
r.jones <r.jones@digipen.edu>
2016-06-25 17:04:29 -0700

    Hey Added directory for pathfinding stuff

---
+++ NavMeshGenerator/Assets/Resources/PathfindingProject/VisualNodeController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VisualNodeController : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        int five = 5;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}
